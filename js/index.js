eb5fe7aadc54fc14da3d1735b32a49f100863a19
choi1002 <4906cjy@naver.com>
2019-01-23 21:21:19 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -80,7 +80,7 @@ function 말추가(){
   말 = document.createElement("li");
   var 대답할것 = 대답후보들[대답][난수생성(0,대답후보들[대답].length-1)];
   let 말할것;
-  if(대답=="학사일정"){
+  /*if(대답=="학사일정"){
 	  var mon=parseInt(입력값.split("/")[0]);
 	  var day=parseInt(입력값.split("/")[1].split(" ")[0]);
 	  대답할것=mon+"월 "+day+"일 ";
@@ -103,7 +103,7 @@ function 말추가(){
 	else{
 		대답할것=급식[day];
 	}
-  }
+  }*/
   말할것 = 대답할것.replace(/<(\/)?([a-zA-Z]*)(\s[a-zA-Z]*=[^>]*)?(\s)*(\/)?>/ig, "");
   말.innerHTML = 대답할것;
   말.setAttribute("class","컴");
